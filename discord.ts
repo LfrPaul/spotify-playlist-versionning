5f73c971d25b48bf07033910e55693f2092923a4
LfrPaul <paulo.azerty59@gmail.com>
2024-05-23 01:24:50 +0200

    [ADD] Discord webhook Refactor in typescript still in progress

---
+++ discord.ts
@@ -0,0 +1,54 @@
+var request = require('request');
+
+const {discord_webhook} = require('./conf/conf');
+
+const colors = {
+    "add": 2021216,
+    "delete": 13842231
+}
+
+const spotify_base_url = "https://open.spotify.com/intl-fr/track/";
+
+function sendWebhook(url: String, addSong: boolean, content: String, url_thumbnail: String) {
+    const body = {
+        "embeds": [
+          {
+            "title": addSong?"Ajout d'un son":"Suppression d'un son",
+            "description": `${content} a été ${addSong?"ajouté à":"supprimé de"} la playlist`,
+            "color": addSong?colors.add:colors.delete,
+            "fields": [],
+            "thumbnail": {
+              "url": url_thumbnail
+            },
+            "url": url
+          }
+        ]
+    }
+
+    console.log(body);
+    
+
+    request.post({
+        url: discord_webhook.url,
+        body: body,
+        headers : {
+            "content-type": "application/json",
+        },
+        json: true
+    })
+
+}
+
+function sendAddSongWebhook(id: String, title: String, artist: String, url_thumbnail: String) {
+    console.log("addSong");
+    sendWebhook(`${spotify_base_url}${id}`, true, `**${title}** de **${artist}**`, url_thumbnail);
+}
+
+function sendDeleteSongWebhook(id: String, title: String, artist: String, url_thumbnail: String) {
+    sendWebhook(`${spotify_base_url}${id}`, false, `**${title}** de **${artist}**`, url_thumbnail);
+}
+
+export {
+    sendDeleteSongWebhook,
+    sendAddSongWebhook
+};
